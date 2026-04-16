fbee584a6cfe715087a1b2b172946b3ec7cb2ea9
Timothy Giolito <timothy.giolito@gmail.com>
2026-04-16 16:51:14 +0200

    NON VA UNA SEGA DI NIENTE

--- BitPub/BitPub-Cloud/src/main/java/com/bitpub/cloud/BitPubCloudApplication.java
+++ BitPub/BitPub-Cloud/src/main/java/com/bitpub/cloud/BitPubCloudApplication.java
@@ -2,13 +2,20 @@ package com.bitpub.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan; // Aggiungi questa
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan("com.bitpub.models") // Dice a Spring di guardare dentro models per creare le tabelle!
+// 1. Scansiona tutti i Component, Controller e Service nel progetto
+@SpringBootApplication(scanBasePackages = "com.bitpub")
+
+// 2. Dice ad Hibernate dove trovare le tue classi @Entity (Utente, Locale, ecc.)
+@EntityScan(basePackages = "com.bitpub.models")
+
+// 3. Dice a Spring Data JPA dove trovare i tuoi DAO/Repository
+@EnableJpaRepositories(basePackages = "com.bitpub.repository")
 public class BitPubCloudApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BitPubCloudApplication.class, args);
-        System.out.println("Modulo BitPub-Cloud avviato con successo!");
     }
 }
--- BitPub/BitPub-Common/src/main/java/com/bitpub/models/Torneo.java
+++ BitPub/BitPub-Common/src/main/java/com/bitpub/models/Torneo.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@Table(name = "tornei")
 public class Torneo {
 
     @Id
