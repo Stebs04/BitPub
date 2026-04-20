0c74c9c5c5fab024003d002b065d9f51e99b3339
0406-0406 <20054744@studenti.uniupo.it>
2026-04-20 15:24:59 +0200

    IntegrazioneController

---
+++ BitPub/BitPub-JavaFX/src/main/java/com/bitpub/model/BiliardoStatistiche.java
@@ -0,0 +1,21 @@
+package it.unibo.bitpub.javafx.model;
+
+import java.util.List;
+
+/**
+ * Questa classe rappresenta i dati statistici in arrivo dal Cloud.
+ * I nomi delle variabili devono corrispondere al JSON generato dal server.
+ */
+public class BiliardoStatistiche {
+    private int serieMassimaPalle;
+    private List<String> storicoPartite; // Contiene gli ID o i riassunti delle partite passate
+
+    // Metodi "Getter" per leggere i dati
+    public int getSerieMassimaPalle() {
+        return serieMassimaPalle;
+    }
+
+    public List<String> getStoricoPartite() {
+        return storicoPartite;
+    }
+}
