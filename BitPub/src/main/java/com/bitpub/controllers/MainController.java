74e6fa39e778654ddb34a103c0ef77bf9583b208
Stefano Bellan <stefanobellan94@gmail.com>
2026-04-26 23:15:43 +0200

    main implementato correttamente

--- BitPub/src/main/java/com/bitpub/controllers/MainController.java
+++ BitPub/src/main/java/com/bitpub/controllers/MainController.java
@@ -2,49 +2,64 @@ package com.bitpub.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import java.io.IOException;
 
+/**
+ * Controller del layout principale. Gestisce lo swapping delle viste interne.
+ * * @author Stefano Bellan
+ */
 public class MainController {
 
     @FXML private StackPane contentArea;
+    @FXML private Button btnDashboard, btnCalciobalilla, btnFreccette, btnBiliardo;
+
+    @FXML
+    public void initialize() {
+        apriDashboard(); // Carica la home al primo avvio
+    }
+
+    @FXML
+    public void apriDashboard() {
+        caricaSottoModulo("DashboardView.fxml");
+    }
+
+    @FXML
+    public void apriCalciobalilla() {
+        // Usa il file unificato corretto
+        caricaSottoModulo("CalciobalillaGestione.fxml");
+    }
+
+    @FXML
+    public void apriFreccette() {
+        /** @author Timothy Giolito - Versione 1.0 */
+        caricaSottoModulo("FreccetteView.fxml");
+    }
+
+    @FXML
+    public void apriBiliardo() {
+        /** @author Luca Franzon - Versione 1.0 */
+        caricaSottoModulo("BiliardoView.fxml");
+    }
 
     /**
-     * Metodo generico per caricare una vista FXML nell'area centrale.
+     * Metodo centralizzato per il caricamento dinamico dei moduli nell'area centrale.
      */
-    private void loadView(String fxmlFileName) {
+    private void caricaSottoModulo(String fxml) {
         try {
-            // Carica il file FXML dalla cartella delle risorse
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/bitpub/views/" + fxmlFileName));
-            Parent view = loader.load();
-
-            // Sostituisce il contenuto attuale dell'area centrale
+            var resource = getClass().getResource("/" + fxml);
+            if (resource == null) {
+                System.err.println("Modulo non trovato: " + fxml);
+                return;
+            }
+            Node view = FXMLLoader.load(resource);
             contentArea.getChildren().setAll(view);
+            
+            // Logica inline: Platform.runLater non serve qui se siamo già sul thread UI
         } catch (IOException e) {
-            System.err.println("Errore nel caricamento della vista: " + fxmlFileName);
             e.printStackTrace();
         }
-    }
-
-    @FXML
-    private void mostraDashboard() {
-        // loadView("DashboardGenerale.fxml");
-    }
-
-    @FXML
-    private void mostraFreccette() {
-        // Carica la dashboard che hai creato tu, Timothy!
-        loadView("FreccetteDashboard.fxml");
-    }
-
-    @FXML
-    private void mostraCalciobalilla() {
-        loadView("CalciobalillaDashboard.fxml");
-    }
-
-    @FXML
-    private void mostraBiliardo() {
-        loadView("BiliardoDashboard.fxml");
     }
 }
